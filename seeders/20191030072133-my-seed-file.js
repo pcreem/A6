ea8b98b4ebec93cb4eef5cd5f86b64c8085be018
pcreem <creempepper@gmail.com>
2019-12-01 01:53:45 +0800

    userImg

--- seeders/20191030072133-my-seed-file.js
+++ seeders/20191030072133-my-seed-file.js
@@ -11,6 +11,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -19,6 +20,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -27,6 +29,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
+      image: faker.image.imageUrl(),
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
